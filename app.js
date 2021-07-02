c05c85ba81d4dace522d3c18ac46c96ff4ff9bfc
hollidavis <hollidavis19@gmail.com>
2021-07-02 15:28:58 -0600

    layout changes, spend points added

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 let beats = 0
 
+let clickUpgrades = {
+  deluxepen: {
+    price: 15,
+    quantity: 0,
+    multiplier: 1
+  },
+  upgradeheadphones: {
+    price: 50,
+    quantity: 0,
+    multiplier: 5
+  },
+  upgradechair: {
+    price: 100,
+    quantity: 0,
+    multiplier: 50
+  },
+};
+
+let automaticUpgrades = {
+  magicpen: {
+    price: 15,
+    quantity: 0,
+    multiplier: 10
+  },
+  coffee: {
+    price: 100,
+    quantity: 0,
+    multiplier: 50
+  },
+  assistant: {
+    price: 1000,
+    quantity: 0,
+    multiplier: 100
+  },
+  ghostwriter: {
+    price: 10000,
+    quantity: 0,
+    multiplier: 1000
+  }
+};
+
 function mine() {
   beats++
   update()
@@ -7,4 +48,14 @@ function mine() {
 
 function update() {
   document.getElementById('beats').innerText = beats.toString()
+}
+
+function buyClickUpgrade(item) {
+  let cost = clickUpgrades[item].price
+  let count = clickUpgrades[item].quantity
+  if (beats >= cost) {
+    count++
+    beats -= cost
+  }
+  update()
 }
